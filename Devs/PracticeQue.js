dbed6e70d5bc85c0ee06d46add8de660fe3ff1c7
siddhik92 <siddhikawade92@gmail.com>
2025-07-05 19:39:25 +0530

    Basic questions

---
+++ Devs/PracticeQue.js
@@ -0,0 +1,21 @@
+// Q1. WAP to greet a person given their first and last name.
+
+// let=Firstname="siddhi";
+// let=Lastname="kawade";
+// console.log("Hello " + Firstname + Lastname + " Welcome to js");
+
+//Q2.WAP that greets a person based on their gender.(if else)
+// let gender="female";
+
+// if(gender=="male")
+//     console.log("Hello madam");
+// else
+// console.log("Hello sir");
+
+// Q3.WAP that counts from 0-1000 and prints (for loop)
+let answer=0;
+for(let i=0;i<=1000;i=i+1){
+    answer=answer+i;
+    
+}
+console.log(answer);
