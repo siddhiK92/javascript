63eea64f924b9a9c04a10b0f1575709488bfc4da
siddhik92 <siddhikawade92@gmail.com>
2025-07-29 07:04:50 +0530

    learn variables&Datatypes in JS

---
+++ Array_ObjectPractice.js
@@ -0,0 +1,19 @@
+// let person1 ="siddhi";
+// let person2 ="rutuja";
+// let peron3="aara";
+
+const personArr = [person1,person2,peron3];
+
+even-odd
+const ages=[21,23,34,56,67];
+for(let i=0;i<ages.length;i++){
+    if(ages[i]%2==0){
+        console.log(ages[i]);
+    }
+}
+
+Object
+const person1 ="siddhi";
+const gender1="female";
+let person2 ="rutuja";
+const gender2="female";
---
+++ Practiceagain.js
@@ -0,0 +1,51 @@
+// variable-var,let,const
+// const a=1;
+// a=2;
+// console.log(3);
+
+// Datatypes
+// let fname="siddhi";
+// let age=18;
+// let isMarried=false;
+
+console.log("this person name is"+ fname + "and their age is" + age);
+console.log("this peron name is ");
+console.log(fname);
+console.log(fname + " is not married");
+
+// if-else stmt-
+
+let fname="siddhi";
+let age=18;
+let isMarried=false;
+
+if(isMarried == true){
+    console.log(fname + " is Married");
+}
+
+if(isMarried == false){
+    console.log(fname + " is not Married");
+}
+
+if(isMarried == true){
+    console.log(fname + " is married");
+}else{
+    console.log(fname + " is not married");
+}
+
+
+// loops-for
+// let answer=1+2+3+4+5+6;
+// console.log(answer);
+
+// answer=answer+0;
+// answer=answer+1;
+// answer=answer+2;... upto 10
+
+let answer=0;
+for(let i=0;i<100;i++){
+     answer=answer+i;
+}
+console.log(answer);
+
+
